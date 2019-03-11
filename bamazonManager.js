c6e1a8e15027a63f4cb811b16a7c2eee8f7c0e97
Dina Khouri <dina.khouri.91@gmail.com>
2019-03-11 13:04:24 -0500

    challenge 2 . part one working

---
+++ bamazonManager.js
@@ -0,0 +1,75 @@
+var mysql = require("mysql");
+var inquire = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  WhatDoYouwantToDo();
+});
+
+function WhatDoYouwantToDo() {
+  inquire
+    .prompt([
+      {
+        type: "list",
+        name: "Do",
+        message: "Would Do you want To Do?",
+        choices: [
+          "View Products For Sale",
+          "View Low Inventory",
+          "Add to inventory",
+          "Add new Product"
+        ]
+      }
+    ])
+    .then(function(answer) {
+      if (answer.Do === "View Products For Sale") {
+        viewProducts();
+      }
+      if (answer.Do === "View Low Inventory") {
+        viewLow();
+      }
+      if (answer.Do === "Add to inventory") {
+        AddInventory();
+      }
+      if (answer.Do === "Add new Product") {
+        AddProduct();
+      }
+    });
+}
+
+function viewProducts() {
+  connection.query("select * from products", function(err, res) {
+    if (err) throw err;
+    var results = [];
+    for (var i = 0; i < res.length; i++) {
+      results.push(
+        res[i].item_id +
+          "||" +
+          "Name: " +
+          res[i].product_name +
+          "||" +
+          "Price: " +
+          res[i].product_price +
+          "||" +
+          "In Stock: " +
+          res[i].stock_quantity
+      );
+    }
+    console.log(results);
+  });
+}
