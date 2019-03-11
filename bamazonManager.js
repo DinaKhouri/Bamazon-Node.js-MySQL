e4bc7ec2759bb03e1d3e90a00b26343b877d725a
Dina Khouri <dina.khouri.91@gmail.com>
2019-03-11 13:31:23 -0500

    challenge 2 part 3 works and updates sql database but then shows error dunno why

--- bamazonManager.js
+++ bamazonManager.js
@@ -97,3 +97,58 @@ function viewLow() {
     console.log(results);
   });
 }
+
+function AddInventory() {
+  connection.query("select * from products", function(err, res) {
+    if (err) throw err;
+    var results = [];
+    for (var i = 0; i < res.length; i++) {
+      results.push(
+        res[i].item_id +
+          "||" +
+          "Name: " +
+          res[i].product_name +
+          "||" +
+          "Price: " +
+          res[i].product_price +
+          "||" +
+          "In Stock: " +
+          res[i].stock_quantity
+      );
+    }
+    inquire
+      .prompt([
+        {
+          type: "list",
+          name: "Picked",
+          message: "Pick the item you want to add to",
+          choices: results
+        }
+      ])
+      .then(function(answer) {
+        var ManagerAnswer = answer.Picked;
+        inquire
+          .prompt([
+            {
+              name: "much",
+              message: "How Many units do you want to add to this item?"
+            }
+          ])
+          .then(function(answer) {
+            var addition = answer.much;
+            for (i = 0; i < results.length; i++) {
+              if (results[i] === ManagerAnswer) {
+                // var ProductPrice = res[i].product_price;
+                // var ProductQuantitiy = res[i].stock_quantity;
+                var ItemID = res[i].item_id;
+              }
+            }
+            //console.log(ItemID);
+            connection.query(
+              "UPDATE products SET stock_quantity = stock_quantity + ? WHERE item_id = ?",
+              [addition, ItemID]
+            );
+          });
+      });
+  });
+}
